ab334da4e736b79ed57acd6a93521e5a8cb44d47
abamba <anais.bamba@outlook.fr>
2016-11-28 15:39:07 +0100

    might lead to override

--- src/inscriptions/Connect.java
+++ src/inscriptions/Connect.java
@@ -8,25 +8,19 @@ public class Connect {
 	  public static void main(String[] args) {      
 
 	    try {
-
+	    // connexion à la bdd avec jdbc
 	      Class.forName("com.mysql.jdbc.Driver");
-	      System.out.println("Driver O.K.");
-
-	      String url = "jdbc:mysql://localhost:3306/inscription";
+	      System.out.println("Driver O.K."); 
+	      
+	      String url = "jdbc:mysql://localhost:3306/inscription"; //localhost de wamp
 	      String user = "root";
 	      String passwd = "";
 
-
 	      Connection conn = DriverManager.getConnection(url, user, passwd);
 
-	      System.out.println("Connexion effective !");         
-
-	         
-
+	      System.out.println("Connexion effective !"); 
 	    } catch (Exception e) {
-
 	      e.printStackTrace();
-
 	    }
 	  }
 }
