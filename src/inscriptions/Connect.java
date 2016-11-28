437d61a6ec21d8894ae263d8add81271c905c0b9
NautilusJungle <iamnautilusjungle@gmail.com>
2016-11-28 15:15:27 +0100

    So is this working?

---
+++ src/inscriptions/Connect.java
@@ -0,0 +1,24 @@
+package inscriptions;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+//CTRL + SHIFT + O pour générer les imports
+public class Connect {
+  public static void main(String[] args) {      
+    try {
+      Class.forName("com.mysql.jdbc.Driver");
+      System.out.println("Driver O.K.");
+
+      String url = "jdbc:mysql://localhost:3306/inscription";
+      String user = "root";
+      String passwd = "";
+
+      Connection conn = DriverManager.getConnection(url, user, passwd);
+      System.out.println("Connexion effective !");         
+         
+    } catch (Exception e) {
+      e.printStackTrace();
+    }      
+  }
+}
