476c7dfe16a34fa314ae9f29c8313d850fb5f4d8
abamba <anais.bamba@outlook.fr>
2016-12-05 01:27:40 +0100

    menu competition

--- src/inscriptions/MenuIndex.java
+++ src/inscriptions/MenuIndex.java
@@ -20,6 +20,27 @@ public class MenuIndex {
 	menuIndex.ajoute(equipe);
 	menuIndex.ajoute(personne);
 	
+	//COMPETITION
+	Option create_compet = new Option("Créer une compétition", "1"); 
+	Option insc_ouvert_compet = new Option("Vérifier si l'inscription est ouverte", "2");
+	Option getListe_compet = new Option("Liste des compétitions", "3");
+	Option getDateCloture_compet = new Option("Date de clôture d'une compétition", "4");
+	Option setDateCloture_compet = new Option("Modifier la date de clôture d'une compétition", "5");
+	Option delete_compet = new Option("Supprimer une compétition", "6");
+	Option getNom_compet = new Option("Nom d'une compétition", "7");
+	Option setNom_compet = new Option("Modifier le nom d'une compétition", "8"); 
+	Option estEnEquipe_compet = new Option("Vérifier si une compétition est en equipe", "9"); 
+	
+	competition.ajoute(create_compet); 
+	competition.ajoute(insc_ouvert_compet);
+	competition.ajoute(getListe_compet);
+	competition.ajoute(getDateCloture_compet);
+	competition.ajoute(setDateCloture_compet); 
+	competition.ajoute(delete_compet);
+	competition.ajoute(getNom_compet); 
+	competition.ajoute(setNom_compet);
+	competition.ajoute(estEnEquipe_compet);
+
 	menuIndex.start();
-	}
+	}	
 }
