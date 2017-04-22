f53893d4e0119defdf5cf777ad4a60789a6f5233
abamba <anais.bamba@outlook.fr>
2017-04-22 20:19:06 +0200

    personal sql start

---
+++ docs/BDD/start2.sql
@@ -0,0 +1,9 @@
+source C:/Users/Blase/git/PPEInscriptions/docs/BDD/create_sport.sql
+source C:/Users/Blase/git/PPEInscriptions/docs/BDD/insert_sport.sql
+source C:/Users/Blase/git/PPEInscriptions/docs/BDD/alter_sport.sql
+source C:/Users/Blase/git/PPEInscriptions/docs/BDD/proc_competition.sql
+source C:/Users/Blase/git/PPEInscriptions/docs/BDD/proc_equipe.sql
+source C:/Users/Blase/git/PPEInscriptions/docs/BDD/proc_inscription.sql
+source C:/Users/Blase/git/PPEInscriptions/docs/BDD/proc_pers.sql
+show tables; 
+select * from candidat;
