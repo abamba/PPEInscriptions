651de43284d82788ff3589854ebe3d364ab2bc07
Nautilus A. Jungle <iamnautilusjungle@gmail.com>
2017-04-24 04:52:33 +0200

    Procs

--- docs/BDD/proc_equipe.sql
+++ docs/BDD/proc_equipe.sql
@@ -3,9 +3,30 @@ DROP PROCEDURE IF EXISTS Composition;
 DELIMITER |
 CREATE PROCEDURE Composition(id_c_s int)
 BEGIN
-    SELECT nom_candidat
+    SELECT id_candidat, nom_candidat, sub, prenom, mail
     FROM CANDIDAT, COMPOSER
     WHERE CANDIDAT.id_candidat = COMPOSER.id_candidat_pers 
     AND id_candidat_squad = id_c_s;
 END |
 DELIMITER ; 
+
+#Get équipes d'un membre
+DROP PROCEDURE IF EXISTS CompositionEquipe;
+DELIMITER |
+CREATE PROCEDURE CompositionEquipe(id_c_s int)
+BEGIN
+    SELECT id_candidat, nom_candidat, sub, prenom, mail
+    FROM CANDIDAT, COMPOSER
+    WHERE CANDIDAT.id_candidat = COMPOSER.id_candidat_squad
+    AND id_candidat_pers = id_c_s;
+END |
+DELIMITER ; 
+
+#Inscrire un membre à une équipe
+DROP PROCEDURE IF EXISTS Compose;
+DELIMITER |
+CREATE PROCEDURE Compose(id_c int,id_s int)
+BEGIN
+    INSERT INTO COMPOSER VALUES (id_s,id_c);
+END |
+DELIMITER ;
